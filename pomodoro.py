3b8f92b06167b3849b826e310117039b4ea8bc35
Nragis <quinn_murphey@me.com>
2020-11-08 08:55:32 -0600

    Finished basic timer function

--- pomodoro.py
+++ pomodoro.py
@@ -3,8 +3,7 @@ from tkinter import ttk
 import random as random
 import time as t
 
-
-class pomodoro(object):
+class pomodoro:
     def __init__(self, parent):
         # Initializing Variables
         self.pomodoroLength = 25
@@ -12,9 +11,13 @@ class pomodoro(object):
         self.longBreakLength = 15
         self.pomodoroInGroup = 4
         self.pomodoroCount = 0
-        self.timerMin = self.pomodoroLength
+        self.timerSec = self.pomodoroLength*60
+
+        self.timeRan = 0
+        self.initialStartTime = 0
 
         self.timerOn = False
+        self.autoPlay = True
         self.timerState = 'POMODORO'
 
         # Initializing mainframe and title
@@ -49,8 +52,8 @@ class pomodoro(object):
         self.taskFrame.grid(row=1,column=0,sticky='N E S W')
         self.doneFrame.grid(row=2,column=0,sticky='N E S W')
 
-        # Creating and placing taskFrame widgets
-        self.timerLabelVar = StringVar(); self.timerLabelVar.set('Timer')
+        # Creating and placing timerFrame widgets
+        self.timerLabelVar = StringVar(); self.timerLabelVar.set("{}:00".format(self.pomodoroLength))
         self.focusLabelVar = StringVar()
         self.pomodoroCountLabelVar = StringVar(); self.pomodoroCountLabelVar.set("1 (1)")
 
@@ -89,7 +92,15 @@ class pomodoro(object):
         self.timerFrame.rowconfigure(3, weight=1)
 
     def reset_timer(self):
-        pass
+        self.timerOn = False
+        self.timerSec = self.pomodoroLength*60
+        self.startstopButton.configure(text='Start Timer', command=self.start_timer)
+        self.timerLabelVar.set("{}:00".format(self.pomodoroLength))
+        self.pomodoroCount = 0
+        self.pomodoroCountLabelVar.set("{}({})".format(int(self.pomodoroCount/self.pomodoroInGroup) + 1, self.pomodoroCount%self.pomodoroInGroup + 1))
+        self.timeRan = 0
+        self.initialStartTime= 0
+        self.focusLabelVar.set("100% Focus")
 
     def open_settings(self):
         pass
@@ -98,51 +109,68 @@ class pomodoro(object):
         self.startstopButton.configure(text='Pause Timer', command=self.pause_timer)
         self.timerOn = True
         self.startTime = t.time()
+        if self.initialStartTime == 0:
+            self.initialStartTime = self.startTime
         self.mainframe.after(0,self.timer)
 
     def pause_timer(self):
-        self.startstopButton.configure(text='Resume Timer', command=self.resume_timer)
+        self.startstopButton.configure(text='Resume Timer', command=self.start_timer)
         self.timerOn = False
-
-    def resume_timer(self):
-        self.startstopButton.configure(text='Pause Timer', command=self.pause_timer)
-        self.timerOn = True
-        self.mainframe.after(0,self.timer)
+        self.timeRan += t.time() - self.startTime
+        self.timerSec -= t.time() - self.startTime
 
     def skip_timer(self):
+        if self.timerOn: self.timeRan += t.time() - self.startTime
         self.timerOn = False
         if self.timerState == 'POMODORO': self.pomodoroCount += 1
         if self.timerState == 'POMODORO' and self.pomodoroCount%self.pomodoroInGroup != 0:
             self.timerState = 'SHORT BREAK'
-            self.timerMin = self.shortBreakLength
+            self.timerSec = self.shortBreakLength*60
+            self.s.configure('TimerFrame.TFrame', background='spring green')
+            self.s.configure('TimerFrame.TLabel', background='spring green')
+            self.s.configure('TimerFrame.TButton', background='spring green')
+            self.s.map('TimerFrame.TButton',background=[('active', 'pale green'),('pressed', 'pale green')])
         elif self.timerState == 'POMODORO' and self.pomodoroCount%self.pomodoroInGroup == 0:
             self.timerState = 'LONG BREAK'
-            self.timerMin = self.longBreakLength
+            self.timerSec = self.longBreakLength*60
+            self.s.configure('TimerFrame.TFrame', background='spring green')
+            self.s.configure('TimerFrame.TLabel', background='spring green')
+            self.s.configure('TimerFrame.TButton', background='spring green')
+            self.s.map('TimerFrame.TButton',background=[('active', 'pale green'),('pressed', 'pale green')])
         elif self.timerState == 'SHORT BREAK' or self.timerState == 'LONG BREAK':
             self.timerState = 'POMODORO'
-            self.timerMin = self.pomodoroLength
+            self.timerSec = self.pomodoroLength*60
+            self.s.configure('TimerFrame.TFrame', background='tomato')
+            self.s.configure('TimerFrame.TLabel', background='tomato')
+            self.s.configure('TimerFrame.TButton', background='tomato')
+            self.s.map('TimerFrame.TButton',background=[('active', 'light coral'),('pressed', 'coral')])
 
-        self.timerLabelVar.set("{}:00".format(self.timerMin))
+        self.timerLabelVar.set("{}:00".format(int(self.timerSec/60)))
         self.pomodoroCountLabelVar.set("{}({})".format(int(self.pomodoroCount/self.pomodoroInGroup) + 1, self.pomodoroCount%self.pomodoroInGroup + 1))
-
+        self.focusLabelVar.set("{}% Focus".format(round(100*(self.timeRan)/(t.time() - self.initialStartTime), 1)))
         self.startstopButton.configure(text='Start Timer', command=self.start_timer)
 
+        if self.autoPlay: self.start_timer()
 
     def timer(self):
         if self.timerOn:
-            minutes = int(float(self.timerMin) - (t.time()-self.startTime)/60)
-            seconds = int(float(self.timerMin)*60 - (t.time()-self.startTime))%60
+            minutes = int(float(self.timerSec)/60 - (t.time()-self.startTime)/60)
+            seconds = int(float(self.timerSec) - (t.time()-self.startTime))%60
             if seconds > 9:
                 self.timerLabelVar.set("{}:{}".format(minutes, seconds))
             else:
                 self.timerLabelVar.set("{}:0{}".format(minutes, seconds))
+
+            self.focusLabelVar.set("{}% Focus".format(round(100*(self.timeRan + t.time() - self.startTime)/(t.time() - self.initialStartTime), 1)))
 
             if minutes < 1 and seconds < 1:
                 self.skip_timer()
             else:
                 self.mainframe.after(200,self.timer)
 
+    def play_sound(self):
 
+        pass
 
 if __name__ == "__main__":
     root = Tk()
